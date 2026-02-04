d0bc214688a824743342fe9d948c85882e57b7ca
Ahmedbenhammouda <ahmedhammouda812@gmail.com>
2026-02-04 12:07:35 +0100

    costumer list final version

--- customer_list.php
+++ customer_list.php
@@ -47,6 +47,8 @@ $search_phone = GETPOST('search_phone', 'alpha');
 $search_email = GETPOST('search_email', 'alpha');
 $search_company = GETPOST('search_company', 'alpha');
 $search_gender = GETPOST('search_gender', 'alpha');
+$search_tax_no = GETPOST('search_tax_no', 'alpha');
+$search_payment_delay = GETPOST('search_payment_delay', 'alpha');
 
 $limit = GETPOST('limit', 'int') ? GETPOST('limit', 'int') : $conf->liste_limit;
 $sortfield = GETPOST('sortfield', 'aZ09comma');
@@ -65,6 +67,11 @@ if (!$sortfield) {
 if (!$sortorder) {
     $sortorder = "ASC";
 }
+
+// Initialize technical objects
+$form = new Form($db);
+$formcompany = new FormCompany($db);
+$hookmanager->initHooks(array('customerlist', 'globalcard'));
 
 // Security check
 restrictedArea($user, 'flotte');
@@ -85,6 +92,31 @@ if (GETPOST('button_removefilter_x', 'alpha') || GETPOST('button_removefilter.x'
     $search_email = '';
     $search_company = '';
     $search_gender = '';
+    $search_tax_no = '';
+    $search_payment_delay = '';
+}
+
+// Handle confirmed delete from list page
+if ($action == 'confirm_delete' && GETPOST('confirm', 'alpha') == 'yes') {
+    $id_to_delete = GETPOST('id', 'int');
+    if ($id_to_delete > 0) {
+        $db->begin();
+        $sql_del = "DELETE FROM ".MAIN_DB_PREFIX."flotte_customer WHERE rowid = ".(int)$id_to_delete." AND entity IN (".getEntity('flotte').")";
+        $resql_del = $db->query($sql_del);
+        if ($resql_del) {
+            // Delete associated files if any
+            $uploadDir = $conf->flotte->dir_output . '/customers/' . $id_to_delete . '/';
+            if (is_dir($uploadDir)) {
+                dol_delete_dir_recursive($uploadDir);
+            }
+            $db->commit();
+            setEventMessages($langs->trans("CustomerDeletedSuccessfully"), null, 'mesgs');
+        } else {
+            $db->rollback();
+            setEventMessages("Error in SQL: ".$db->lasterror(), null, 'errors');
+        }
+    }
+    $action = 'list';
 }
 
 // Build and execute select
@@ -114,11 +146,17 @@ if ($search_company) {
 if ($search_gender) {
     $sql .= " AND t.gender = '".$db->escape($search_gender)."'";
 }
+if ($search_tax_no) {
+    $sql .= " AND t.tax_no LIKE '%".$db->escape($search_tax_no)."%'";
+}
+if ($search_payment_delay) {
+    $sql .= " AND t.payment_delay LIKE '%".$db->escape($search_payment_delay)."%'";
+}
 
 $sql .= $db->order($sortfield, $sortorder);
 
 // Count total nb of records
-$sqlcount = str_replace('SELECT t.rowid, t.ref, t.firstname, t.lastname, t.phone, t.email, t.company_name, t.tax_no, t.payment_delay, t.gender', 'SELECT COUNT(*) as nb', $sql);
+$sqlcount = preg_replace('/^SELECT[^,]+(,\s*[^,]+)*\s+FROM/', 'SELECT COUNT(*) as nb FROM', $sql);
 $resql = $db->query($sqlcount);
 $nbtotalofrecords = 0;
 if ($resql) {
@@ -129,7 +167,6 @@ if ($resql) {
 $sql .= $db->plimit($limit + 1, $offset);
 $resql = $db->query($sql);
 
-$form = new Form($db);
 $num = 0;
 if ($resql) {
     $num = $db->num_rows($resql);
@@ -141,15 +178,22 @@ llxHeader('', $langs->trans("CustomersList"), '');
 // Page title and buttons
 $newCardButton = '';
 if ($user->rights->flotte->write) {
-    $newCardButton = dolGetButtonTitle($langs->trans('NewCustomer'), '', 'fa fa-plus-circle', dol_buildpath('/flotte/customer_card.php', 1).'?action=create', '', $user->rights->flotte->read);
+    $newCardButton = dolGetButtonTitle($langs->trans('New Customer'), '', 'fa fa-plus-circle', dol_buildpath('/flotte/customer_card.php', 1).'?action=create', '', $user->rights->flotte->read);
 }
 
-print load_fiche_titre($langs->trans("CustomersList"), $newCardButton, 'user');
+// Show delete confirmation dialog if requested
+if ($action == 'delete') {
+    $id_to_delete = GETPOST('id', 'int');
+    if ($id_to_delete > 0) {
+        $formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$id_to_delete, $langs->trans('DeleteCustomer'), $langs->trans('ConfirmDeleteCustomer'), 'confirm_delete', '', 0, 1);
+        print $formconfirm;
+    }
+}
 
 // Actions bar
 print '<div class="tabsAction">'."\n";
 if ($user->rights->flotte->write) {
-    print '<a class="butAction" href="'.dol_buildpath('/flotte/customer_card.php', 1).'?action=create">'.$langs->trans("NewCustomer").'</a>'."\n";
+    print '<a class="butAction" href="'.dol_buildpath('/flotte/customer_card.php', 1).'?action=create">'.$langs->trans("New Customer").'</a>'."\n";
 }
 if ($user->rights->flotte->read) {
     print '<a class="butAction" href="'.dol_buildpath('/flotte/customer_list.php', 1).'?action=export">'.$langs->trans("Export").'</a>'."\n";
@@ -165,6 +209,8 @@ if (!empty($search_phone))         $param .= '&search_phone='.urlencode($search_
 if (!empty($search_email))         $param .= '&search_email='.urlencode($search_email);
 if (!empty($search_company))       $param .= '&search_company='.urlencode($search_company);
 if (!empty($search_gender))        $param .= '&search_gender='.urlencode($search_gender);
+if (!empty($search_tax_no))        $param .= '&search_tax_no='.urlencode($search_tax_no);
+if (!empty($search_payment_delay)) $param .= '&search_payment_delay='.urlencode($search_payment_delay);
 
 // Search form
 print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
@@ -176,12 +222,12 @@ print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
 print '<input type="hidden" name="page" value="'.$page.'">';
 
 // Print barre liste
-print_barre_liste($langs->trans("CustomersList"), $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, '', $num, $nbtotalofrecords, 'user', 0, $newCardButton, '', $limit, 0, 0, 1);
+print_barre_liste($langs->trans("CustomersList"), $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, '', $num, $nbtotalofrecords, '', 0);
 
 print '<div class="div-table-responsive">';
 print '<table class="tagtable liste" id="tablelines">'."\n";
 
-// Search fields
+// Search fields - Matching driver_list.php structure
 print '<tr class="liste_titre_filter">';
 print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_ref" value="'.dol_escape_htmltag($search_ref).'"></td>';
 print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_firstname" value="'.dol_escape_htmltag($search_firstname).'"></td>';
@@ -189,9 +235,9 @@ print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name=
 print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_phone" value="'.dol_escape_htmltag($search_phone).'"></td>';
 print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_email" value="'.dol_escape_htmltag($search_email).'"></td>';
 print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_company" value="'.dol_escape_htmltag($search_company).'"></td>';
-print '<td class="liste_titre"></td>'; // Tax No
-print '<td class="liste_titre"></td>'; // Payment Delay
-print '<td class="liste_titre">';
+print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_tax_no" value="'.dol_escape_htmltag($search_tax_no).'"></td>';
+print '<td class="liste_titre"><input type="text" class="flat maxwidth100" name="search_payment_delay" value="'.dol_escape_htmltag($search_payment_delay).'"></td>';
+print '<td class="liste_titre center">';
 $genderarray = array(''=>'', 'male'=>$langs->trans('Male'), 'female'=>$langs->trans('Female'), 'other'=>$langs->trans('Other'));
 print $form->selectarray('search_gender', $genderarray, $search_gender, 0, 0, 0, '', 0, 0, 0, '', 'search_gender width100 onrightofpage');
 print '</td>';
@@ -201,7 +247,7 @@ print $searchpicto;
 print '</td>';
 print '</tr>'."\n";
 
-// Table headers
+// Table headers - Matching driver_list.php structure
 print '<tr class="liste_titre">';
 print_liste_field_titre("Ref", $_SERVER["PHP_SELF"], "t.ref", "", $param, '', $sortfield, $sortorder);
 print_liste_field_titre("FirstName", $_SERVER["PHP_SELF"], "t.firstname", "", $param, '', $sortfield, $sortorder);
@@ -211,11 +257,16 @@ print_liste_field_titre("Email", $_SERVER["PHP_SELF"], "t.email", "", $param, ''
 print_liste_field_titre("CompanyName", $_SERVER["PHP_SELF"], "t.company_name", "", $param, '', $sortfield, $sortorder);
 print_liste_field_titre("TaxNumber", $_SERVER["PHP_SELF"], "t.tax_no", "", $param, '', $sortfield, $sortorder);
 print_liste_field_titre("PaymentDelay", $_SERVER["PHP_SELF"], "t.payment_delay", "", $param, '', $sortfield, $sortorder);
-print_liste_field_titre("Gender", $_SERVER["PHP_SELF"], "t.gender", "", $param, '', $sortfield, $sortorder);
+// Manually create centered header for gender column like driver list status column
+print '<td class="liste_titre center">';
+print '<a class="reposition" href="'.$_SERVER["PHP_SELF"].'?sortfield=t.gender&sortorder='.($sortfield == 't.gender' && $sortorder == 'ASC' ? 'DESC' : 'ASC').$param.'">';
+print $langs->trans("Gender");
+if ($sortfield == 't.gender') print img_picto('', 'sort'.strtolower($sortorder));
+print '</a></td>';
 print_liste_field_titre("Action", $_SERVER["PHP_SELF"], "", "", "", '', '', '', 'maxwidthsearch ');
 print '</tr>'."\n";
 
-// Display data
+// Display data - Matching driver_list.php style
 if ($resql && $num > 0) {
     $i = 0;
     while ($i < min($num, $limit)) {
@@ -224,33 +275,39 @@ if ($resql && $num > 0) {
         
         print '<tr class="oddeven">';
         
-        // Reference
+        // Reference - REMOVED THE ICON
         print '<td class="nowrap"><a href="'.dol_buildpath('/flotte/customer_card.php', 1).'?id='.$obj->rowid.'">';
-        print img_object($langs->trans("ShowCustomer"), "user", 'class="pictofixedwidth"');
         print '<strong>'.dol_escape_htmltag($obj->ref).'</strong></a></td>';
         
         // First Name
-        print '<td>'.dol_escape_htmltag($obj->firstname).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->firstname ?: '-').'</td>';
         
         // Last Name
-        print '<td>'.dol_escape_htmltag($obj->lastname).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->lastname ?: '-').'</td>';
         
         // Phone
-        print '<td>'.dol_escape_htmltag($obj->phone).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->phone ?: '-').'</td>';
         
         // Email
-        print '<td>'.dol_escape_htmltag($obj->email).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->email ?: '-').'</td>';
         
         // Company Name
-        print '<td>'.dol_escape_htmltag($obj->company_name).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->company_name ?: '-').'</td>';
         
         // Tax Number
-        print '<td>'.dol_escape_htmltag($obj->tax_no).'</td>';
+        print '<td>'.dol_escape_htmltag($obj->tax_no ?: '-').'</td>';
         
-        // Payment Delay
-        print '<td>'.($obj->payment_delay ? $obj->payment_delay . ' ' . $langs->trans('Days') : '').'</td>';
+        // Payment Delay - Formatted like driver list vehicle info
+        print '<td>';
+        if ($obj->payment_delay) {
+            print dol_escape_htmltag($obj->payment_delay);
+            print '<br><small style="color: #666;">'.$langs->trans('Days').'</small>';
+        } else {
+            print '<em style="color: #999;">'.$langs->trans("NotSpecified").'</em>';
+        }
+        print '</td>';
         
-        // Gender
+        // Gender - Centered with status style like driver list
         print '<td class="center">';
         if ($obj->gender == 'male') {
             print dolGetStatus($langs->trans('Male'), '', '', 'status4', 1);
@@ -258,19 +315,18 @@ if ($resql && $num > 0) {
             print dolGetStatus($langs->trans('Female'), '', '', 'status8', 1);
         } elseif ($obj->gender == 'other') {
             print dolGetStatus($langs->trans('Other'), '', '', 'status9', 1);
+        } else {
+            print '<em style="color: #999;">'.$langs->trans("NotSpecified").'</em>';
         }
         print '</td>';
         
-        // Actions
+        // Actions - Same style as driver list
         print '<td class="nowrap center">';
-        if ($user->rights->flotte->read) {
-            print '<a class="editfielda" href="'.dol_buildpath('/flotte/customer_card.php', 1).'?id='.$obj->rowid.'" title="'.$langs->trans("View").'">'.img_view($langs->trans("View")).'</a>';
-        }
         if ($user->rights->flotte->write) {
             print '<a class="editfielda" href="'.dol_buildpath('/flotte/customer_card.php', 1).'?id='.$obj->rowid.'&action=edit" title="'.$langs->trans("Edit").'">'.img_edit($langs->trans("Edit")).'</a>';
         }
         if ($user->rights->flotte->delete) {
-            print '<a class="editfielda" href="'.dol_buildpath('/flotte/customer_card.php', 1).'?id='.$obj->rowid.'&action=delete&token='.newToken().'" title="'.$langs->trans("Delete").'">'.img_delete($langs->trans("Delete")).'</a>';
+            print '<a class="editfielda" href="'.dol_buildpath('/flotte/customer_list.php', 1).'?action=delete&id='.$obj->rowid.'&token='.newToken().'" title="'.$langs->trans("Delete").'">'.img_delete($langs->trans("Delete")).'</a>';
         }
         print '</td>';
         
@@ -287,7 +343,7 @@ print '</div>'."\n";
 
 print '</form>'."\n";
 
-// Print pagination
+// Print pagination - Same as driver list
 if ($nbtotalofrecords > $limit) {
     print_barre_liste('', $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, '', $num, $nbtotalofrecords, '', 0, '', '', $limit, 1);
 }
