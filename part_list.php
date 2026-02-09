4633db5936ac6a29b91bf152cdc8a82b8a11ab1a
owek4ever <aliasabdelkader23@gmail.com>
2026-02-09 11:17:42 +0100

    fixed part list file

--- part_list.php
+++ part_list.php
@@ -302,7 +302,6 @@ if ($resql && $num > 0) {
         
         // Reference
         print '<td class="nowrap"><a href="'.dol_buildpath('/flotte/part_card.php', 1).'?id='.$obj->rowid.'">';
-        print img_object($langs->trans("ShowPart"), "generic", 'class="pictofixedwidth"');
         print '<strong>'.dol_escape_htmltag($obj->ref).'</strong></a></td>';
         
         // Title
